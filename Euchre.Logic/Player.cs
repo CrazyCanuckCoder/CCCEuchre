10a8eda9bb473357aee1bff77ef522213afcd056
CrazyCanuckCoder <sjfletcher@protonmail.ch>
2025-07-15 15:01:59 -0400

    Added documentation to the base Player class.

--- Euchre.Logic/Player.cs
+++ Euchre.Logic/Player.cs
@@ -4,8 +4,16 @@ using static Euchre.Logic.Constants;
 
 namespace Euchre.Logic;
 
+/// <summary>
+/// The base class for all players in the game of Euchre.
+/// </summary>
 public abstract class Player : IPlayer
 {
+    /// <summary>
+    /// Creates a new player with the specified name and whether they are human or not.
+    /// </summary>
+    /// <param name="name">The name of the player.</param>
+    /// <param name="isHuman">True if the player is a human and false for automated players.</param>
     public Player(string name, bool isHuman)
     {
         Name = name;
@@ -13,10 +21,19 @@ public abstract class Player : IPlayer
         IsHuman = isHuman;
     }
 
+    /// <summary>
+    /// The cards in the player's hand.
+    /// </summary>
     public List<Card> Hand { get; protected set; }
 
+    /// <summary>
+    /// True if the player is a human player, false if the player is an automated player.
+    /// </summary>
     public bool IsHuman { get; protected set; }
 
+    /// <summary>
+    /// The name of the player.
+    /// </summary>
     public string Name { get; protected set; }
 
     /// <summary>
@@ -40,11 +57,20 @@ public abstract class Player : IPlayer
         Hand.Add(card);
     }
 
+    /// <summary>
+    /// Clears the player's hand, removing all cards.
+    /// </summary>
     public void ClearHand()
     {
         Hand.Clear();
     }
 
+    /// <summary>
+    /// Removes a card from the player's hand at the specified index to simulate playing a card.
+    /// </summary>
+    /// <param name="index">The index of the card to play.</param>
+    /// <returns>The card played.</returns>
+    /// <exception cref="ArgumentOutOfRangeException"></exception>
     public Card PlayCard(int index)
     {
         if (index < 0 || index >= Hand.Count)
@@ -57,11 +83,37 @@ public abstract class Player : IPlayer
         return card;
     }
 
+    /// <summary>
+    /// Prompts the player to call trump for the game.
+    /// </summary>
+    /// <param name="kitty">The card from the kitty indicating which suit cannot be called trump.</param>
+    /// <returns>The trump suit selected by the user, or <see langword="null"/> if no trump suit is 
+    /// selected which indicates the automated player will pass.</returns>
     public abstract Suit? CallTrump(Card kitty);
 
+    /// <summary>
+    /// Determines which card in the player's hand will be replaced by the kitty's upturned card.
+    /// </summary>
+    /// <param name="kitty">The upturned card from the kitty.</param>
+    /// <param name="trump">The trump suit for the current round.</param>
     public abstract void DiscardForKitty(Card kitty, Suit trump);
 
+    /// <summary>
+    /// Determines whether to order up the given card during the bidding phase.
+    /// </summary>
+    /// <param name="kitty">The card being considered for ordering up.</param>
+    /// <param name="isDealer">A boolean value indicating whether the current player is the dealer.</param>
+    /// <returns><see langword="true"/> if the player decides to order up the card; otherwise, 
+    /// <see langword="false"/>.</returns>
     public abstract bool OrderUp(Card kitty, bool isDealer);
 
+    /// <summary>
+    /// Selects the card to play for the current trick based on the provided game state.
+    /// </summary>
+    /// <param name="trick">The current trick containing the cards played so far.</param>
+    /// <param name="trump">The trump suit for the game.</param>
+    /// <param name="leadSuit">The suit that was led for the trick, or <see langword="null"/> if no suit has 
+    /// been led yet.</param>
+    /// <returns>The card selected to play in the current trick.</returns>
     public abstract Card SelectCardToPlay(Trick trick, Suit trump, Suit? leadSuit);
 }
