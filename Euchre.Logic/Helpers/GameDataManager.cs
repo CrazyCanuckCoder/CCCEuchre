a2c1ceb8a8521d89eebb6c7e4b0dd663b70e49e2
CrazyCanuckCoder <sjfletcher@protonmail.ch>
2025-09-09 11:02:33 -0400

    Changed the properties of the GameDataManager class to be accessible from classes within its own assembly.

--- Euchre.Logic/Helpers/GameDataManager.cs
+++ Euchre.Logic/Helpers/GameDataManager.cs
@@ -26,55 +26,65 @@ public class GameDataManager
     /// The list of players in the game. The first two players are on one team, and the last two players are 
     /// on the other team.
     /// </summary>
-    public IPlayer[]? Players { get; set; }
+    public IPlayer[]? Players { get; internal set; }
 
     /// <summary>
     /// The deck of cards used in the game, which is shuffled and dealt to players at the start of each round.
     /// </summary>
-    public Deck Deck { get; set; }
+    public Deck Deck { get; internal set; }
 
     /// <summary>
     /// The kitty card, which is the upturned card from the kitty that players can order up or exchange if
     /// they are the dealer.
     /// </summary>
-    public Card? Kitty { get; set; }
+    public Card? Kitty { get; internal set; }
 
     /// <summary>
     /// The trump suit for the current round. This is determined during the bidding phase when a player calls
     /// trump or orders up the kitty card.
     /// </summary>
-    public Suit? Trump { get; set; }
+    public Suit? Trump { get; internal set; }
 
     /// <summary>
     /// The dealer for the current round.
     /// </summary>
-    public IPlayer? Dealer { get; set; }
+    public IPlayer? Dealer { get; internal set; }
 
     /// <summary>
     /// The player who called trump for the current round.
     /// </summary>
-    public IPlayer? TrumpCaller { get; set; }
+    public IPlayer? TrumpCaller { get; internal set; }
 
     /// <summary>
     /// Stores the scores for each team in the game. Team 0 (Players 0,2), Team 1 (Players 1,3).
     /// </summary>
-    public int[] TeamScores { get; set; }
+    public int[] TeamScores { get; internal set; }
 
     /// <summary>
     /// The list of tricks played in the current round. Each trick contains the cards played by each player.
     /// </summary>
-    public List<Trick> CurrentRoundTricks { get; set; }
+    public List<Trick> CurrentRoundTricks { get; internal set; }
 
     /// <summary>
     /// True if the game is currently in a hand where one player has declared they are going alone, and false
     /// if otherwise.
     /// </summary>
-    public bool GoingAlone { get; set; }
+    public bool GoingAlone { get; internal set; }
 
     /// <summary>
     /// The player who is going alone in the current hand, if applicable.
     /// </summary>
-    public IPlayer? AlonePlayer { get; set; }
+    public IPlayer? AlonePlayer { get; internal set; }
+
+    /// <summary>
+    /// Indicates the player who is to play the next card in the current trick.
+    /// </summary>
+    public IPlayer NextTrickPlayer { get; internal set; }
+
+    /// <summary>
+    /// True to indicate the game should be restarted, which can be used to reset the game state.
+    /// </summary>
+    public bool RestartGame { get; internal set; }
 
     /// <summary>
     /// Saves the current game data to a file.
