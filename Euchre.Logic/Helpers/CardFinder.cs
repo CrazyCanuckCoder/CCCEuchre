840d298d27c92946f07fa4735bb68e0d87b05f30
CrazyCanuckCoder <sjfletcher@protonmail.ch>
2025-09-22 15:48:16 -0400

    Added documentation to CardFinder class.

--- Euchre.Logic/Helpers/CardFinder.cs
+++ Euchre.Logic/Helpers/CardFinder.cs
@@ -3,29 +3,64 @@ using Euchre.Logic.Components;
 
 namespace Euchre.Logic.Helpers;
 
+/// <summary>
+/// Finds a card or cards in a collection based on various criteria.
+/// </summary>
 public static class CardFinder
 {
-
+    /// <summary>
+    /// Determines whether the specified collection of cards contains at least one card of the given suit.
+    /// </summary>
+    /// <param name="cards">The collection of cards to search. Cannot be <see langword="null"/>.</param>
+    /// <param name="suit">The suit to check for in the collection.</param>
+    /// <returns>True if the collection contains at least one card of the specified suit; otherwise, false.</returns>
     public static bool HasACardOfSuit(List<Card> cards, Suit suit)
     {
         return cards.Any(c => c.Suit == suit);
     }
 
+    /// <summary>
+    /// Determines whether the specified collection of cards contains at least one card of the trump suit.
+    /// </summary>
+    /// <param name="cards">The collection of cards to evaluate. Cannot be null.</param>
+    /// <param name="trump">The suit to check for as the trump suit.</param>
+    /// <returns>True if the collection contains at least one card with the specified trump suit; otherwise,
+    /// false.</returns>
     public static bool HasTrump(List<Card> cards, Suit trump)
     {
         return cards.Any(c => c.EffectiveSuit(trump) == trump);
     }
 
+    /// <summary>
+    /// Determines whether the specified collection of cards contains a bower.
+    /// </summary>
+    /// <param name="cards">The collection of cards to search.</param>
+    /// <param name="trump">The trump suit used to identify the bower.</param>
+    /// <returns>True if the collection contains a bower for the specified trump suit; otherwise, false.</returns>
     public static bool HasBower(List<Card> cards, Suit trump)
     {
         return cards.Any(c => c.IsBower(trump));
     }
 
+    /// <summary>
+    /// Determines whether the specified collection of cards contains at least one card with a suit that is
+    /// not the trump suit.
+    /// </summary>
+    /// <param name="cards">The collection of cards to evaluate.</param>
+    /// <param name="trump">The trump suit used to determine the off suit cards.</param>
+    /// <returns>True if the collection contains at least one card with a suit that is not the trump suit; 
+    /// otherwise, false.</returns>
     public static bool HasOffSuit(List<Card> cards, Suit trump)
     {
         return cards.Any(c => c.EffectiveSuit(trump) != trump);
     }
 
+    /// <summary>
+    /// Retrieves the highest ranked card from the specified list that is not of the trump suit.
+    /// </summary>
+    /// <param name="cards">The list of cards to evaluate. Cannot be null.</param>
+    /// <param name="trump">The trump suit to exclude from consideration.</param>
+    /// <returns>The highest ranked card that is not of the trump suit, or null if no such card exists.</returns>
     public static Card? GetHighestOffSuitCard(List<Card> cards, Suit trump)
     {
         return cards
@@ -34,6 +69,12 @@ public static class CardFinder
                 .FirstOrNull();
     }
 
+    /// <summary>
+    /// Retrieves the lowest ranked card from the specified list that is not of the trump suit.
+    /// </summary>
+    /// <param name="cards">The list of cards to evaluate. Cannot be null.</param>
+    /// <param name="trump">The trump suit to exclude from consideration.</param>
+    /// <returns>The lowest ranked card that is not of the trump suit, or null if no such card exists.</returns>
     public static Card? GetLowestOffSuitCard(List<Card> cards, Suit trump)
     {
         return cards
@@ -42,16 +83,34 @@ public static class CardFinder
                 .LastOrNull();
     }
 
+    /// <summary>
+    /// Retrieves the highest trump card from the specified list of cards.
+    /// </summary>
+    /// <param name="cards">The list of cards to evaluate. Cannot be null.</param>
+    /// <param name="trump">The suit considered trump.</param>
+    /// <returns>The highest ranked card of the trump suit, or null if no such card exists.</returns>
     public static Card? GetHighestTrumpCard(List<Card> cards, Suit trump)
     {
         return cards.OrderByDescending(c => c.GetTrickValue(trump, trump)).FirstOrNull();
     }
 
+    /// <summary>
+    /// Retrieves the lowest trump card from the specified list of cards.
+    /// </summary>
+    /// <param name="cards">The list of cards to evaluate. Cannot be null.</param>
+    /// <param name="trump">The suit considered trump.</param>
+    /// <returns>The lowest ranked card of the trump suit, or null if no such card exists.</returns>
     public static Card? GetLowestTrumpCard(List<Card> cards, Suit trump)
     {
         return cards.OrderByDescending(c => c.GetTrickValue(trump, trump)).LastOrNull();
     }
 
+    /// <summary>
+    /// Retrieves the highest ranked card of the specified suit from the given list of cards.
+    /// </summary>
+    /// <param name="cards">The list of cards to evaluate. Cannot be null.</param>
+    /// <param name="suit">The suit of the card to look for.</param>
+    /// <returns>The highest ranked card of the specified suit, or null if no such card exists.</returns>
     public static Card? GetHighestCardOfSuit(List<Card> cards, Suit? suit)
     {
         return cards
@@ -60,6 +119,12 @@ public static class CardFinder
                 .FirstOrNull();
     }
 
+    /// <summary>
+    /// Retrieves the highest bower card from the specified list of cards.
+    /// </summary>
+    /// <param name="cards">The list of cards to evaluate. Cannot be null.</param>
+    /// <param name="trump">The suit considered trump.</param>
+    /// <returns>The highest bower card or null if one is not found.</returns>
     public static Card? GetHighestBowerCard(List<Card> cards, Suit trump)
     {
         return cards
@@ -68,11 +133,23 @@ public static class CardFinder
                 .FirstOrNull();
     }
 
+    /// <summary>
+    /// Counts the number of cards in the specified list that match the given suit.
+    /// </summary>
+    /// <param name="cards">The list of cards to evaluate. Cannot be null.</param>
+    /// <param name="suit">The suit of the cards to count.  Does not work for trump.</param>
+    /// <returns>The number of cards found for the specified suit.</returns>
     public static int CountCardsOfSuit(List<Card> cards, Suit suit)
     {
         return cards.Count(c => c.Suit == suit);
     }
 
+    /// <summary>
+    /// Counts the number of cards in the specified list that match the given rank.
+    /// </summary>
+    /// <param name="cards">The list of cards to evaluate. Cannot be null.</param>
+    /// <param name="rank">The rank of cards to count.</param>
+    /// <returns>The number of cards found for the specified rank.</returns>
     public static int CountCardsOfRank(List<Card> cards, Rank rank)
     {
         return cards.Count(c => c.Rank == rank);
